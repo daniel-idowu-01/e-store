f698503d0604e26d931fd6c1e4c938aa6e37fa5e
daniel-idowu-01 <danielidowu414@gmail.com>
2023-10-15 22:17:32 +0100

    create order summary

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -3,7 +3,8 @@ import CartContext from '../context/CartContext'
 
 const CheckOut = () => {
 
-  const { items } =  useContext(CartContext);
+  const { items } = useContext(CartContext);
+  const inputStyle = 'w-full border outline-none px-4 py-2 rounded-md'
 
   return (
     <section className='flex flex-col md:flex-row md:justify-between py-8 px-10 md:px-20'>
@@ -12,16 +13,41 @@ const CheckOut = () => {
         <h3 className='text-xl font-semibold mb-2'>Billing Details</h3>
         <form action="" className='flex flex-col gap-5'>
             <div className='flex gap-2'>
-                <input className='w-full border outline-none px-4 py-2 rounded-md' type="text" name="first_name" id="first_name" placeholder='First Name' />
-                <input className='w-full border outline-none px-4 py-2 rounded-md' type="text" name="last_name" id="last_name" placeholder='Last Name' />
+                <input 
+                  className={inputStyle} 
+                  type="text"
+                  name="first_name"
+                  id="first_name"
+                  placeholder='First Name'
+                />
+                <input 
+                className={inputStyle} 
+                  type="text"
+                  name="last_name"
+                  id="last_name"
+                  placeholder='Last Name'
+                />
             </div>
             
             <div className='flex gap-2'>
-                <input className='w-full border outline-none px-4 py-2 rounded-md' type="email" name="email" id="email" placeholder='Email' />
-                <input className='w-full border outline-none px-4 py-2 rounded-md' type="password" name="password" id="password" placeholder='Phone Number' />
+                <input 
+                className={inputStyle} 
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder='Email'
+                />
+                <input 
+                className={inputStyle} 
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder='Phone Number'
+                />
             </div>
 
-            <select className='w-full border outline-none px-4 py-2 rounded-md' name="country" id="country">
+            <select 
+            className={inputStyle} name="country" id="country">
               <option disabled>Select Country</option>
               <option value="United Kingdom">United Kingdom</option>
               <option value="United States">United States</option>
@@ -30,10 +56,17 @@ const CheckOut = () => {
               <option value="Other">Other</option>
           </select>
 
-          <input className='w-full border outline-none px-4 py-2 rounded-md' type="text" name="address" id="address" placeholder='Address' />
+          <input 
+            className={inputStyle} 
+            type="text"
+            name="address"
+            id="address"
+            placeholder='Address'
+          />
 
           <div className='flex gap-2'>
-            <select className='w-full border outline-none px-4 py-2 rounded-md' name="country" id="country">
+            <select 
+            className={inputStyle} name="country" id="country">
               <option disabled>Select Country</option>
               <option value="United Kingdom">United Kingdom</option>
               <option value="United States">United States</option>
@@ -41,53 +74,58 @@ const CheckOut = () => {
               <option value="South Africa">South Africa</option>
               <option value="Other">Other</option>
             </select>
-            <input className='w-full border outline-none px-4 py-2 rounded-md' type="number" name="postcode" id="postcode" placeholder='Postcode / ZIP' />
+            <input 
+              className={inputStyle} 
+              type="number"
+              name="postcode"
+              id="postcode"
+              placeholder='Postcode / ZIP'
+            />
           </div>
         </form>
       </article>
 
       {/* order summary */}
-      <article>
+      <article className='flex flex-col gap-3'>
         <h3 className='text-xl font-semibold mb-2'>Order Summary</h3>
-      <table className='w-full border-collapse'>
-        <thead>
-          <tr>
-            <th>
-              Product
-            </th>
-            <th>
-              Quantity
-            </th>
-            <th>
-              Price
-            </th>
-            <th>
-              
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          { items.length 
-            ? 
-          items.map((item) => (
-            <tr className='tr'>
-              <td className='p-2'>
-                {item.name}
-              </td>
-              <td className='p-2'>
-                {item.quantity}
-              </td>
-              <td className='p-2'>
-                ${item.totalPrice}
-              </td>
+        <table className='border p-10 rounded-md'>
+          <thead>
+            <tr>
+              <th>
+                Product
+              </th>
+              <th>
+                Quantity
+              </th>
+              <th>
+                Price
+              </th>
             </tr>
+          </thead>
+          <tbody className='text-center'>
+            { items.length 
+              ? 
+            items.map((item) => (
+              <tr className='tr'>
+                <td className='p-2 text-sm w-64'>
+                  {item.name}
+                </td>
+                <td className='p-2 text-sm'>
+                  {item.quantity}
+                </td>
+                <td className='p-2 text-sm'>
+                  ${item.totalPrice}
+                </td>
+              </tr>
+              )
             )
-          )
-            : 
-          <p>No items in cart</p> 
-          }  
+              : 
+            <p>No items in cart</p> 
+            }  
           </tbody>
         </table>
+
+        <button className=' bg-coolblue rounded-full text-white w-full py-2'>Place Order Now</button>
       </article>
     </section>
   )
